c033cc3221560c23e31e6acae76d22561c458c62
Robert <sk.robchik@gmail.com>
2019-12-09 10:15:29 -0800

    seems to be working and minimax gives correct eval

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,34 @@ extern crate adversarial_search;
 
 mod tictactoe;
 
-use tictactoe::Game;
 use adversarial_search::prelude::minimax;
 use tictactoe::Coordinate;
+use tictactoe::Game;
 
 use std::io::stdin;
 
-macro_rules! to_search_player {
-    ($x:expr) => {
-        match $x{
-            tictactoe::Player::CROSS => adversarial_search::prelude::Player::MAX,
-            tictactoe::Player::CIRCLE => adversarial_search::prelude::Player::MIN,
-        }
-    };
+fn to_search_player(player: tictactoe::Player) -> adversarial_search::prelude::Player {
+    match player {
+        tictactoe::Player::CROSS => adversarial_search::prelude::Player::MAX,
+        tictactoe::Player::CIRCLE => adversarial_search::prelude::Player::MIN,
+    }
+}
+fn to_search_terminality(outcome: Option<tictactoe::GameOutcome>) -> Option<f32> {
+    if outcome.is_none() {
+        return None;
+    }
+    match outcome.unwrap() {
+        tictactoe::GameOutcome::CROSS_WIN => Some(std::f32::INFINITY),
+        tictactoe::GameOutcome::CIRCLE_WIN => Some(-std::f32::INFINITY),
+        tictactoe::GameOutcome::DRAW => Some(0.0),
+    }
 }
 
 fn main() {
     let mut game: Game = Game::new();
     println!("{}", game.to_string());
-    
-    for _ in 0..9{
+
+    for _ in 0..9 {
         let move_coordinate = prompt_move(&game);
         game.make_move(move_coordinate);
         println!("{}", game.to_string());
@@ -29,15 +37,11 @@ fn main() {
             &game,
             10,
             &|n| n.get_children(),
-            &|n| {
-                let winning = n.is_winning();
-                if winning.is_none(){
-                    return None;
-                }
-                Some(to_search_player!(winning.unwrap()))
+            &|n| to_search_terminality(n.get_outcome()),
+            &|_| {
+                return 0;
             },
-            &|_| {return 0;},
-            to_search_player!(game.get_game_turn())
+            to_search_player(game.get_game_turn()),
         );
         println!("eval: {}", eval);
     }
@@ -60,15 +64,13 @@ fn prompt_move(game: &Game) -> Coordinate {
             }
             let index = index.unwrap();
             let coord = (index / 3, index % 3);
-            if available_moves.binary_search(&coord).is_ok(){
+            if available_moves.binary_search(&coord).is_ok() {
                 return coord;
-            }
-            else {
+            } else {
                 println!("That tile is occupied");
                 continue;
             }
-        }
-        else {
+        } else {
             println!("Invalid string");
         }
     }
--- src/tictactoe.rs
+++ src/tictactoe.rs
@@ -4,53 +4,68 @@ pub type Board = [Line; 3];
 pub type Coordinate = (usize, usize);
 
 macro_rules! return_winnable {
-    ($x:expr) => (
+    ($x:expr) => {
         let line = $x;
         let winning = Game::is_winning_line(line);
         if winning.is_some() {
             return winning;
         }
-    );
+    };
+}
+
+macro_rules! player_to_game_outcome {
+    ($x:expr) => {
+        match $x {
+            Player::CROSS => GameOutcome::CROSS_WIN,
+            Player::CIRCLE => GameOutcome::CIRCLE_WIN,
+        }
+    };
 }
 
 #[derive(PartialEq, Debug, Copy, Clone)]
-pub enum TileState{
+pub enum TileState {
     CROSS,
     CIRCLE,
     EMPTY,
 }
 
 #[derive(Copy, Clone)]
-pub enum Player{
+pub enum Player {
     CROSS,
     CIRCLE,
 }
 
-pub struct Game{
+pub enum GameOutcome {
+    CROSS_WIN,
+    CIRCLE_WIN,
+    DRAW,
+}
+
+pub struct Game {
     board: Board,
     game_turn: Player,
 }
 
-impl Game{
-    pub fn new() -> Game{
-        Game{
+impl Game {
+    pub fn new() -> Game {
+        Game {
             board: [[TileState::EMPTY; 3]; 3],
             game_turn: Player::CROSS,
         }
     }
 
-    pub fn from_board(board: Board, player: Player) -> Game{
-        Game{
+    pub fn from_board(board: Board, player: Player) -> Game {
+        Game {
             board: board,
             game_turn: player,
         }
     }
 
-    pub fn empty_tiles(&self) -> Vec<Coordinate>{
+    pub fn empty_tiles(&self) -> Vec<Coordinate> {
         let mut tiles = Vec::new();
-        for i in 0..3{
-            for j in 0..3{
-                if self.board[i][j] == TileState::EMPTY{
+        for i in 0..3 {
+            for j in 0..3 {
+                if self.board[i][j] == TileState::EMPTY {
                     tiles.push((i, j));
                 }
             }
@@ -58,32 +73,32 @@ impl Game{
         tiles
     }
 
-    pub fn get_game_turn(&self) -> Player{
+    pub fn get_game_turn(&self) -> Player {
         self.game_turn
     }
 
     pub fn make_move(&mut self, coord: Coordinate) {
-        self.board[coord.0][coord.1] = match self.game_turn{
+        self.board[coord.0][coord.1] = match self.game_turn {
             Player::CROSS => TileState::CROSS,
             Player::CIRCLE => TileState::CIRCLE,
         };
-        self.game_turn = match self.game_turn{
+        self.game_turn = match self.game_turn {
             Player::CROSS => Player::CIRCLE,
             Player::CIRCLE => Player::CROSS,
         };
     }
 
-    pub fn get_children(&self) -> Vec<Game>{
+    pub fn get_children(&self) -> Vec<Game> {
         let mut children: Vec<Game> = Vec::new();
         let empty_tiles = self.empty_tiles();
-        for tile in empty_tiles{
+        for tile in empty_tiles {
             let mut child: Game = Game::from_board(self.board, self.game_turn);
             child.make_move(tile);
             children.push(child);
         }
         children
     }
-    pub fn get_board(&self) -> Board{
+    pub fn get_board(&self) -> Board {
         self.board
     }
 
@@ -104,19 +119,18 @@ impl Game{
     }
 
     fn is_winning_line(line: Line) -> Option<Player> {
-        if line[0] == line[1] && line[1] == line[2] && line[0] != TileState::EMPTY
-        {
-            return Some( match line[0] {
+        if line[0] == line[1] && line[1] == line[2] && line[0] != TileState::EMPTY {
+            return Some(match line[0] {
                 TileState::CROSS => Player::CROSS,
                 TileState::CIRCLE => Player::CIRCLE,
                 TileState::EMPTY => unreachable!(),
-            })
+            });
         }
         None
     }
 
-    pub fn is_winning(&self) -> Option<Player> {
-        for i in 0..3{
+    fn is_winning(&self) -> Option<Player> {
+        for i in 0..3 {
             return_winnable!(self.get_row(i));
             return_winnable!(self.get_column(i));
         }
@@ -125,13 +139,36 @@ impl Game{
         None
     }
 
+    fn count_empty_tiles(&self) -> u8 {
+        let mut n = 0;
+        for i in 0..3 {
+            for j in 0..3 {
+                if self.board[i][j] == TileState::EMPTY {
+                    n = n + 1;
+                }
+            }
+        }
+        n
+    }
+
+    pub fn get_outcome(&self) -> Option<GameOutcome> {
+        if self.count_empty_tiles() != 0 {
+            return None;
+        }
+        let winning = self.is_winning();
+        if winning.is_none() {
+            return Some(GameOutcome::DRAW);
+        }
+        Some(player_to_game_outcome!(winning.unwrap()))
+    }
+
     pub fn to_string(&self) -> String {
         let mut output: String = String::new();
-        for i in 0..3{
+        for i in 0..3 {
             output.push_str("| ");
-            for j in 0..3{
+            for j in 0..3 {
                 let tile = self.board[i][j];
-                output.push_str(match tile{
+                output.push_str(match tile {
                     TileState::EMPTY => "   ",
                     TileState::CIRCLE => " o ",
                     TileState::CROSS => " x ",
